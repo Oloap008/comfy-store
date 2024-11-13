1809e8e32a5cf5465ec722e4d1c0d2c6eb13bb1d
Paolo <paolohenry008@gmail.com>
2024-11-13 10:02:10 +0800

    Add pagination to the products page. Mark products page as complete.

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,56 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom";
+
 function PaginationContainer() {
-  return <div>PaginationContainer</div>;
+  const {
+    meta: {
+      pagination: { pageCount, page },
+    },
+  } = useLoaderData();
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  function handlePageChange(pageNumber) {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+
+    navigate(`${pathname}?${searchParams.toString()}`);
+  }
+
+  if (pageCount < 2) return null;
+
+  return (
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}
+        >
+          Prev
+        </button>
+        {Array.from({ length: pageCount }, (_, index) => (
+          <button
+            key={index}
+            type="button"
+            onClick={() => handlePageChange(index + 1)}
+            className={`btn btn-xs sm:btn-md border-none join-item ${
+              index + 1 === page ? "bg-base-300 border-base-300" : ""
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          className="btn btn-xs sm:btn-md join-item"
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === pageCount}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default PaginationContainer;
